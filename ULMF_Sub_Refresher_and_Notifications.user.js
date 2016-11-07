4008a17f363052db938c5cd9aacc7ef3eb3d25e9
emerladCoder <emerladCoder@gmail.com>
2016-11-07 17:08:39 -0600

    Audio

--- ULMF_Sub_Refresher_and_Notifications.user.js
+++ ULMF_Sub_Refresher_and_Notifications.user.js
@@ -3,7 +3,7 @@
 // @namespace   ulmfrefresher
 // @description Refreshes Page and Checks for Unread Subs
 // @include     http://www.ulmf.org/bbs/subscription.php*
-// @version     1.0.1
+// @version     1.0.2
 // @downloadURL https://github.com/emerladCoder/ULMF_Subscription_Refresher/raw/master/ULMF_Sub_Refresher_and_Notifications.user.js
 // @grant       unsafeWindow
 // ==/UserScript==
@@ -53,7 +53,7 @@ var SubRefresher = {
 
             // make audio element
             var audio = document.createElement("audio");
-            audio.src = audio_source;
+            audio.src = SubRefresher.audio_source;
             audio.preload = "auto";
             // attach it to the page
             $("body").append(audio);
