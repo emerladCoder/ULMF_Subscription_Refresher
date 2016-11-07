45fc9be0271a0c692fef0b6d0b888851fd7e0ba8
emerladCoder <emerladCoder@gmail.com>
2016-11-07 16:56:15 -0600

    Add downloadURL

--- ULMF_Sub_Refresher_and_Notifications.user.js
+++ ULMF_Sub_Refresher_and_Notifications.user.js
@@ -4,6 +4,7 @@
 // @description Refreshes Page and Checks for Unread Subs
 // @include     http://www.ulmf.org/bbs/subscription.php*
 // @version     1.0.0
+// @downloadURL https://github.com/emerladCoder/ULMF_Subscription_Refresher/raw/master/ULMF_Sub_Refresher_and_Notifications.user.js
 // @grant       unsafeWindow
 // ==/UserScript==
 
